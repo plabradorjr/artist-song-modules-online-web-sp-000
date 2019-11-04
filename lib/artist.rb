982c675322313059b5d9f06739c10e91916c2dc4
Pancho Labrador <plabradorjr@gmail.com>
2019-11-04 08:34:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require_relative 'lib/concerns/Memorable'
 
 class Artist
   attr_accessor :name
@@ -19,13 +20,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
+  #
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
