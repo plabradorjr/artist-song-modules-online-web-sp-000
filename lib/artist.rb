b4ab0cf35c6e59956ac3ada7d70c8e0a26e87aef
Pancho Labrador <plabradorjr@gmail.com>
2019-11-04 08:39:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 require 'pry'
 require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
 
 class Artist
   attr_accessor :name
@@ -14,9 +15,9 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
